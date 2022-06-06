68ea00d23a5ee5d25015e560b8ddd9502eb0c8a5
injet-zhou <zhouyingjie@sinkcode.cn>
2022-06-06 21:27:00 +0800

    add login

---
+++ src/views/login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="form" id="form">
+    <div class="field email">
+      <div class="icon"></div>
+      <input
+        class="input"
+        id="email"
+        type="email"
+        placeholder="Email"
+        autocomplete="off"
+      />
+    </div>
+    <div class="field password">
+      <div class="icon"></div>
+      <input
+        class="input"
+        id="password"
+        type="password"
+        placeholder="Password"
+      />
+    </div>
+    <button class="button" id="submit">
+      LOGIN
+      <div class="side-top-bottom"></div>
+      <div class="side-left-right"></div></button
+    ><small>Fill in the form</small>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
